04ad8bf7908a5a05434b5b237272c4914be5518c
AlvaradoAdam15 <adamalvarado@iesebre.com>
2016-01-13 15:16:15 +0100

    Slide image background with backstretch

---
+++ assets/js/backstretch.js
@@ -0,0 +1,10 @@
+/**
+ * Created by adam on 13/01/16.
+ */
+$(document).ready(function() {
+    $.backstretch([
+        "assets/img/background.png"
+        , "assets/img/background3.png"
+        , "assets/img/background2.png"
+    ], {duration: 3000, fade: 750});
+});
